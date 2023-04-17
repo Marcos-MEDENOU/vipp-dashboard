a433a94e3306904a828602324b089b98afbaa4f1
marcosmedenou <mmedenou@vippinterstis.com>
2023-04-17 21:10:04 +0200

    @marcos

---
+++ app/Filament/Resources/ArticlesResource.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Filament\Resources;
+use App\Filament\Resources\ArticlesResource\Pages;
+use App\Filament\Resources\ArticlesResource\RelationManagers;
+use App\Models\Articles;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\View;
+
+
+class ArticlesResource extends Resource
+{
+    protected static ?string $model = Articles::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                // RichEditor::make('content')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListArticles::route('/'),
+            'create' => Pages\CreateArticles::route('/create'),
+            'edit' => Pages\EditArticles::route('/{record}/edit'),
+        ];
+    }    
+}
